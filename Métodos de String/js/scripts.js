e74a194769fb8d8d70a3357566743ed8958948db
José Anderson <jose.andersonjr2@gmail.com>
2022-09-06 17:58:15 -0300

    Métodos de Strign

---
+++ Métodos de String/js/scripts.js
@@ -0,0 +1,21 @@
+// length
+let nome = 'Anderson'
+console.log(nome.length)
+
+let obj = 'bola'
+console.log(obj.length)
+
+// indexOf
+console.log(nome[2])
+
+let frase = 'Lorem Ipsom'
+console.log(frase.indexOf("Ipsom"))
+
+// slice
+
+let ipsom = frase.slice(6, 11)
+console.log(ipsom)
+
+// replace
+ let novaFrase = frase.replace('Ipsom', 'Epsom')
+ console.log(novaFrase)
