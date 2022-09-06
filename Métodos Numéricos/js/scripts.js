5d1a77dd2f66bc65e1b611055dd37fe8b3f7ba1f
José Anderson <jose.andersonjr2@gmail.com>
2022-09-06 17:50:25 -0300

    Métodos numéricos

---
+++ Métodos Numéricos/js/scripts.js
@@ -0,0 +1,22 @@
+// parseFloat
+console.log(parseFloat('12.999'))
+console.log(Number.parseFloat('12.999'))
+
+//parseInt
+console.log(parseInt('10'))
+console.log(parseInt(19.0))
+
+
+//toFixed
+console.log(0923.232323.toFixed(1))
+console.log(93939.3434.toFixed(3))
+ 
+//isNaN
+console.log(isNaN(12))
+console.log(isNaN('teste'))
+console.log(isNaN('124'))
+
+// MAX_VALUE AND MIN_VALUE
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+
