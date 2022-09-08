db15f8572f5d5814d3fabd044eb68387466743e8
José Anderson <jose.andersonjr12@gmail.com>
2022-09-08 07:05:09 -0300

    Funções de String pt2

---
+++ Escopo/js/scripts.js
@@ -0,0 +1,10 @@
+let x = 5
+x = 12
+
+console.log(x)
+
+if (true){
+    let x = 20
+    console.log(x)
+}
+console.log(x)
--- Métodos de String/js/scripts.js
+++ Métodos de String/js/scripts.js
@@ -17,5 +17,33 @@ let ipsom = frase.slice(6, 11)
 console.log(ipsom)
 
 // replace
- let novaFrase = frase.replace('Ipsom', 'Epsom')
- console.log(novaFrase)
+let novaFrase = frase.replace('Ipsom', 'Epsom')
+console.log(novaFrase)
+
+// toLowerCase
+
+let fraseNormal = 'Esta é a frase que vamos manipular'
+
+let fraseCaixaAlta = fraseNormal.toUpperCase()
+console.log(fraseNormal)
+console.log(fraseCaixaAlta)
+console.log(fraseCaixaAlta.toLowerCase())
+
+// trim
+
+let nome2 = '            José              '
+let nome2Trim = nome2.trim()
+console.log(nome2)
+console.log(nome2Trim)
+
+// split
+console.log(fraseNormal.split(' '))
+let tags = 'PHP, JavaScript, HTML, CSS'
+console.log(tags.split(', '))
+
+// lastIndexOf
+
+let frase3 = 'Eu quero a última palavra teste desta frase de teste'
+
+console.log(frase3.indexOf('teste'))
+console.log(frase3.lastIndexOf('teste'))
