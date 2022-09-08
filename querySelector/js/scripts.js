27af5c0e3c98f5bc589fac66d43bc9f6e9758704
José Anderson <jose.andersonjr2@gmail.com>
2022-09-08 19:17:36 -0300

    Acessando o DOM

---
+++ querySelector/js/scripts.js
@@ -0,0 +1,14 @@
+let itensClasse = document.getElementsByClassName('item')
+console.log(itensClasse)
+
+// querySelectorAll
+let itensQuery = document.querySelectorAll('#lista2 li')
+console.log(itensQuery)
+
+let listaItens2 = document.querySelectorAll('#lista2 .item')
+console.log(listaItens2)
+
+// querySelector
+let listaItens3 = document.querySelector('#lista')
+console.log(listaItens3)
+
