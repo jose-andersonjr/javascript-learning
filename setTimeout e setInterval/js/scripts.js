986a1c61742f684118b37e474b6f54d6af7a7900
José Anderson <jose.andersonjr2@gmail.com>
2022-09-09 12:25:36 -0300

    setTimeout e setInterval

---
+++ setTimeout e setInterval/js/scripts.js
@@ -0,0 +1,15 @@
+// setTimeout
+console.log('Antes do timeout')
+
+setTimeout(function (){
+    console.log('Testando o setTimeout')
+}, 2000)
+
+console.log('Depois do timeout')
+
+// setInterval
+setInterval(function() {
+    
+    console.log('Testando interval')
+
+}, 1000)
