cc5ed87af5140b3ec6a212e160312454e2c41649
José Anderson <jose.andersonjr2@gmail.com>
2022-09-09 11:57:24 -0300

    Propriedades de document

---
+++ Propriedades de document/js/scripts.js
@@ -0,0 +1,16 @@
+// Propriedades document
+console.log(document.body)
+
+console.log(document.head)
+
+console.log(document.links[0])
+console.log(document.links[1])
+
+document.links[0].textContent = 'Twitter'
+
+console.log(document.URL)
+
+console.log(document.title) //Esse title é o principal da página
+
+document.title = 'Alterando o título do site'
+
