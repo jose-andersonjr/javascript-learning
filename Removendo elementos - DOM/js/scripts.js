a0696159e4696da19a3302cbdfd9bf8a551e9435
José Anderson <jose.andersonjr2@gmail.com>
2022-09-09 09:08:50 -0300

    Removendo elementos - DOM

---
+++ Removendo elementos - DOM/js/scripts.js
@@ -0,0 +1,10 @@
+// Removendo elemento filho
+let container = document.querySelector('#container')
+
+let paragrafo = document.querySelector('#container p')
+container.removeChild(paragrafo)
+
+// Remover o elemento em si
+let subtitle = document.querySelector('subtitle')
+
+subtitle.remove()
