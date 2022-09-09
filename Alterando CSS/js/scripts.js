317a0df5d9e52e56032f221cc39cd549f7433463
José Anderson <jose.andersonjr2@gmail.com>
2022-09-09 11:46:26 -0300

    Alterando o CSS

---
+++ Alterando CSS/js/scripts.js
@@ -0,0 +1,16 @@
+// Selecionar elemento
+let title = document.querySelector('#title')
+
+// Adiciona o estilo
+title.style.color = 'red'
+//O style sendo inserido pelo JavaScript tem preferência maior que a do CSS
+
+//background-color
+title.style.backgroundColor = 'yellow'
+
+// Selecionar elemento
+let subtitle = document.querySelector('.subtitle')
+
+// Adicionar vários estilos 
+subtitle.style.cssText = 'color: blue; background-color: pink; font-size: 50px;'
+
