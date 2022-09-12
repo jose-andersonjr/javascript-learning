ce359edff2d7c7b77b32ff9f4984067a2eec6985
José Anderson <jose.andersonjr2@gmail.com>
2022-09-12 09:38:17 -0300

    Eventos keydown e keyup

---
+++ Eventos keydown e keyup/js/scripts.js
@@ -0,0 +1,15 @@
+// keydown
+document.addEventListener('keydown', function(event){
+    if (event.key === 'Enter'){
+        console.log('Apertou Enter')
+    }
+    
+})
+
+// keyup
+document.addEventListener('keyup', function(event){
+
+    if(event.key === 'Enter'){
+        console.log('Soltou o Enter')
+    }
+})
