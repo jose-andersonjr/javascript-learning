aee71b82b630a1fa232be71475ebf8a720e74709
José Anderson <jose.andersonjr2@gmail.com>
2022-09-12 08:39:50 -0300

    Revisão setInterval e setTimeout

--- setTimeout e setInterval/js/scripts.js
+++ setTimeout e setInterval/js/scripts.js
@@ -1,17 +1,22 @@
-//clearTimeout na prática
-let x = 0
-let myTimer = setTimeout(function (){
-    console.log("O x 0 é 0")
-}, 1500)
+//setTimeout na prática
 
-x = 5
+//O primeiro parâmetro é a função a ser executada e o segundo parâmetro é intervalo de tempo para ser executado.
+//Uma função pode ser configurada para ser executada a cada 5segundos
+var segundos = 0
+console.log('Testando insert serTimeout')
 
-if (x > 0) {
-    clearTimeout(myTimer)
-    console.log('O x passou de 0')
-}
+console.log('Antes do setTimeout')
+setTimeout(function(){
+    
+    segundos += 2
+    console.log(segundos + ' segundos se passaram...')
 
-// clearSetInterval na prática
-let myInterval = setInternal(function () {
-    console.log()
-})
+}, 2000)
+console.log('Depois do setTimeout')
+//O setTimeout é um função assíncrona, ela espera a sua condição de tempo para ser executada, não abre junto das outras linhas de comando
+
+setInterval(function(){
+
+    console.log('Testando setInterval')
+
+}, 1000)
