77c2f0baef37b807df1894b62d8af332e15ab299
José Anderson <jose.andersonjr2@gmail.com>
2022-09-12 08:16:26 -0300

    Revisão callback functions

--- Callback functions/js/scripts.js
+++ Callback functions/js/scripts.js
@@ -1,18 +1,20 @@
-function exibir(num){
-    console.log('A operação resultou em: ' + num)
+//Permite executar uma função depois de uma ação
+
+//A função exibir está recebendo o return das funções som a emultiplicacao
+function exibir(numero){
+    console.log('A operação resultou em: ' + numero)
 }
 
-function soma(a, b, callback) {
-    let op = a + b
-    callback(op)
+function soma(a, b, cb){
+    var resultado = a + b
+    cb(resultado)
 }
 
-function multiplicacao(a, b, cb) {
-    let op = a * b
-    cb(op)
+function multiplicacao(a, b, cb){
+    var resultado = a * b
+    cb(resultado)
 }
 
 soma(2, 2, exibir)
 
-multiplicacao(2, 4, exibir)
-
+multiplicacao(2, 4, exibir) //Callback é a chamada de uma função usando o resultado de outra
